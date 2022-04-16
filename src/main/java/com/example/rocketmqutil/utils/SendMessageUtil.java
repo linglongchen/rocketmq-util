609d43f7983dd5d5d6903940e6b8e3489c222e47
chenlingl <cll022034@gmail.com>
2022-04-16 23:25:11 +0800

    Desc:优化代码！

--- src/main/java/com/example/rocketmqutil/utils/SendMessageUtil.java
+++ src/main/java/com/example/rocketmqutil/utils/SendMessageUtil.java
@@ -1,9 +1,11 @@
 package com.example.rocketmqutil.utils;
 
 import com.alibaba.fastjson.JSON;
+import io.netty.util.internal.ThrowableUtil;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.exception.RemotingException;
@@ -12,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author chenlingl
@@ -31,19 +35,59 @@ public class SendMessageUtil {
 
     /**
      * sync send normal message
-     * @param topic
-     * @param tag
-     * @param o
+     * @param topic 消息主题
+     * @param tag 消息标签
+     * @param msgBody 消息体
      * @return
      * @throws MQBrokerException
      * @throws RemotingException
      * @throws InterruptedException
      * @throws MQClientException
      */
-    public SendResult sendSyncMessage(String topic,String tag,Object o) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
-        Message sendMsg = new Message(topic, tag, JSON.toJSONBytes(o));
+    public void sendSyncMessage(String topic,String tag,Object msgBody) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+        Message sendMsg = new Message(topic, tag, JSON.toJSONBytes(msgBody));
         SendResult sendResult = defaultMQProducer.send(sendMsg);
-        log.info("==========send message result：{}================",sendResult.toString());
-        return sendResult;
+        log.info("==========send message success,topic:{},tag:{},result：{}================",topic,tag,JSON.toJSONString(sendMsg));
+    }
+
+    /**
+     * 异步发送消息
+     * @param topic 消息主题
+     * @param tag 消息标签
+     * @param msgBody 消息体
+     * @throws RemotingException
+     * @throws InterruptedException
+     * @throws MQClientException
+     */
+    public void sendASyncMessage(String topic, String tag, Object msgBody) throws RemotingException, InterruptedException, MQClientException {
+        Message sendMsg = new Message(topic, tag, JSON.toJSONBytes(msgBody));
+        defaultMQProducer.send(sendMsg, new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                log.info("==========send message success,topic:{},tag:{},result：{}================",topic,tag,JSON.toJSONString(sendResult));
+            }
+            @Override
+            public void onException(Throwable e) {
+                log.info("==========send message success,topic:{},tag:{},result：{}================",topic,tag, ThrowableUtil.stackTraceToString(e));
+            }
+        });
+    }
+
+    /**
+     * 延迟消息
+     * @param topic 消息主题
+     * @param tag 消息标签
+     * @param delayTime 延迟时间
+     * @param msgBody 消息体
+     * @throws MQBrokerException
+     * @throws RemotingException
+     * @throws InterruptedException
+     * @throws MQClientException
+     */
+    public void sendDelayMessage(String topic,String tag,long delayTime,Object msgBody) throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+        Message sendMsg = new Message(topic, tag, JSON.toJSONBytes(msgBody));
+        long curTime = System.currentTimeMillis() + delayTime;
+        SendResult sendResult = defaultMQProducer.send(sendMsg);
+        log.info("==========send message success,topic:{},tag:{},result：{}================",topic,tag,JSON.toJSONString(sendResult));
     }
 }
